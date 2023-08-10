c0fba756b072ee22869dc8aee077faa72dcd24d0
elesawy <elesawy928@gmail.com>
2023-08-11 00:40:04 +0300

    create custom note item

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:const EdgeInsets.symmetric(horizontal: 16,vertical: 24),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text(
+                'create your notes with elesawy',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16,right: 24),
+            child: Text(
+              'may11/2023',
+              style: TextStyle(color: Colors.black.withOpacity(0.5),fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/note_view_body.dart
+++ lib/views/widgets/note_view_body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'custom_app_bar.dart';
+import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -12,12 +13,18 @@ class NotesViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
-        children:const [
-          SizedBox(height: 24,),
+        children: const [
+          SizedBox(
+            height: 24,
+          ),
           CustomAppBar(),
+          SizedBox(
+            height: 24,
+          ),
+          NoteItem()
         ],
       ),
     );
-    ;
   }
 }
+
