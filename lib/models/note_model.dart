bad9310ae4fd532eb736f951fcafe66d71543eb8
elesawy <elesawy928@gmail.com>
2023-08-15 18:33:14 +0300

    add hive package

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,12 +1,12 @@
 class NoteModel {
   final String title;
-  final String subtitle;
+  final String subTitle;
   final String date;
   final int color;
 
   NoteModel(
       {required this.title,
-      required this.subtitle,
+      required this.subTitle,
       required this.date,
       required this.color});
 }
